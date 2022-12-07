44b491937213d99ed79addefab0960971322d594
hyang7 <hyang7@trip.com>
2022-12-07 19:18:02 +0800

    log

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const egg = require('egg');
 
 const workers = Number(process.argv[2] || require('os').cpus().length);
 
+console.log('===', __dirname)
+
 egg.startCluster({
     workers,
     baseDir: __dirname
